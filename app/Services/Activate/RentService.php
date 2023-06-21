a4f337ef640f5b3f9a266a0d77698ee80ec7e002
evans86 <evans86@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>
2023-06-21 23:56:15 +0300

    update orders

--- app/Services/Activate/RentService.php
+++ app/Services/Activate/RentService.php
@@ -335,9 +335,7 @@ class RentService extends MainService
             $update_codes[] = $new_codes;
             $rentOrder->codes = $update_codes;
         }else{
-//            $update_codes[] = $rentOrder->codes;
-//            $update_codes[][] = $new_codes;
-            $rentOrder->codes =+ $new_codes;
+            array_push($rentOrder->codes, $new_codes);
         }
 
 
