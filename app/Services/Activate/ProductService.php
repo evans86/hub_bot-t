4850a15c3d7da8df2f6788a0779e6b2492dabad4
evans <evandaniil85@gmail.com>
2024-09-09 23:02:13 +0300

    add max price

--- app/Services/Activate/ProductService.php
+++ app/Services/Activate/ProductService.php
@@ -70,11 +70,11 @@ class ProductService extends MainService
         $apiRate = ProductService::formingRublePrice();
 //        dd($apiRate);
 
-        $services = \Cache::get('services_' . $country);
-        if ($services === null) {
+//        $services = \Cache::get('services_' . $country);
+//        if ($services === null) {
             $services = $smsActivate->getPrices($country);
-            \Cache::put('services_' . $country, $services, 15);
-        }
+//            \Cache::put('services_' . $country, $services, 15);
+//        }
 
         $services = current($services);
 //        dd($services);
